1d33ea53d906cb6ce6853a790e30aae9955eee9d
Александр Евгеньевич Малеев <72337323+AlexMalFrost@users.noreply.github.com>
2023-05-02 11:00:49 +0300

    Update asyncActions.ts
    delete moc api address

--- src/redux/slices/asyncActions.ts
+++ src/redux/slices/asyncActions.ts
@@ -7,7 +7,7 @@ export const fetchPizzas = createAsyncThunk<Pizza[], Record<string, string>>(
   async (params) => {
     const { order, sortBy, category, search, currentPage } = params;
     const { data } = await axios.get<Pizza[]>(
-      `https://6429b940ebb1476fcc4f9b86.mockapi.io/items?page=${currentPage}&limit=4&${category}${search}&sortBy=${sortBy}&order=${order}`,
+      `https://.mockapi.io/items?page=${currentPage}&limit=4&${category}${search}&sortBy=${sortBy}&order=${order}`,
     );
     return data;
   },
